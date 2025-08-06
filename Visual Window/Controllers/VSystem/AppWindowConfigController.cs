7caf192012b98d548ce6817a79b588259377a12d
nanaminato <bethaniebell8474@gmail.com>
2025-08-06 20:44:41 +0800

    edit

---
+++ Visual Window/Controllers/VSystem/AppWindowConfigController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Repositories.VSystem;
+
+namespace WebApplication1.Controllers.VSystem;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class AppWindowConfigController: Controller
+{
+    public AppWindowConfigController()
+    {
+        
+    }
+
+    [HttpGet("installed-apps")]
+    public IActionResult GetAllInstalledApps()
+    {
+        return Ok(ProjectAppDefines.GetAppWindowConfigs());
+    }
+}
---
+++ Visual Window/Repositories/VSystem/ProjectAppDefines.cs
@@ -0,0 +1,65 @@
+﻿using WebApplication1.Models.Apps;
+
+namespace WebApplication1.Repositories.VSystem;
+
+public class ProjectAppDefines
+{
+    public static List<AppWindowConfig> GetAppWindowConfigs()
+    {
+        List<AppWindowConfig> appWindowConfigs =
+        [
+            new()
+            {
+                AppId = "file-browser",
+                AppName = "文件浏览器",
+                AppType = AppType.SystemApp,
+                Icon = new AppIcon()
+                {
+                    IconType = IconType.MaterialIcon,
+                    Name = "folder"
+                },
+                IsSingleton = false,
+                PreferredSize = new PreferredSize()
+                {
+                    Width = 800,
+                    Height = 600
+                }
+            },
+            new()
+            {
+                AppId = "terminal",
+                AppName = "终端",
+                AppType = AppType.SystemApp,
+                Icon = new AppIcon()
+                {
+                    IconType = IconType.MaterialIcon,
+                    Name = "terminal"
+                },
+                IsSingleton = false,
+                PreferredSize = new PreferredSize()
+                {
+                    Width = 800,
+                    Height = 600
+                }
+            },
+            new()
+            {
+                AppId = "docker",
+                AppName = "Docker 管理",
+                AppType = AppType.NormalApp,
+                Icon = new AppIcon()
+                {
+                    IconType = IconType.MaterialIcon,
+                    Name = "docker"
+                },
+                IsSingleton = true,
+                PreferredSize = new PreferredSize()
+                {
+                    Width = 800,
+                    Height = 600
+                }
+            }
+        ];
+        return appWindowConfigs;
+    }
+}
